aed1260d4d8b7a3da851be4984a1ef54c33a2a1d
Luis Camilo Jimenez Alvarez <luisca1985@gmail.com>
2022-12-17 15:51:35 -0500

    venv, dev pkgs & wagtail start inertia_site

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Page using wagtail
+## Installation
+Inside the folder, run the setup steps necessary for any Django project:
+
+```bash
+pip install -r requirements/dev.txt
+python manage.py migrate
+python manage.py createsuperuser
+python manage.py runserver
+```
+
+## References
+- [Wagtail Documentation](https://docs.wagtail.org/en/stable/)
+  - [Getting started](https://docs.wagtail.org/en/stable/getting_started/index.html)
---
+++ inertia_site/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-v(h#3y%dp--8m-o%^#pui$!%x1f@#&#+s-r5h^al1y8636%n6="
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
